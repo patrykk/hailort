52f58eca378b30b2f59dfd814f9e9ae38786943c
Blake Blackshear <blake.blackshear@gmail.com>
2025-04-12 05:59:52 -0500

    Switch to native builds and submodules for easier management (#1)
    * add release build
    
    * build python 3.11 wheels too
    
    * use ubuntu to avoid building python from source
    
    * switching to submodule

--- hailort/libhailort/bindings/gstreamer/find_libs_for_gstreamer_linux.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(VERSION 3.5.0)
-
-find_package(PkgConfig REQUIRED)
-pkg_search_module(GLIB REQUIRED glib-2.0)
-pkg_search_module(GSTREAMER REQUIRED gstreamer-1.0)
-pkg_search_module(GSTREAMER_BASE REQUIRED gstreamer-base-1.0)
-pkg_search_module(GSTREAMER_VIDEO REQUIRED gstreamer-video-1.0)
-pkg_search_module(GSTREAMER_PLUGINS_BASE REQUIRED gstreamer-plugins-base-1.0)
